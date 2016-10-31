3d7acadf21a595d17325e8a44a464ed232465df1
Vineeth K <vineeth@utexas.edu>
2016-10-31 12:12:20 -0500

    Adding Authentication, LogIn and SignUp activities

---
+++ CoopCompanion/app/src/main/java/com/panjwani/ovais/coopcompanion/SignUpActivity.java
@@ -0,0 +1,19 @@
+package com.panjwani.ovais.coopcompanion;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+}
