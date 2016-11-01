8067bec5373bf53e66afaef7e70c995bd62e37b9
Vineeth K <vineeth@utexas.edu>
2016-11-01 12:15:58 -0500

    adding layout for signup activity

--- CoopCompanion/app/src/main/java/com/panjwani/ovais/coopcompanion/Authentication.java
+++ CoopCompanion/app/src/main/java/com/panjwani/ovais/coopcompanion/Authentication.java
@@ -1,8 +1,11 @@
 package com.panjwani.ovais.coopcompanion;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 
 public class Authentication extends AppCompatActivity {
 
@@ -12,6 +15,26 @@ public class Authentication extends AppCompatActivity {
         setContentView(R.layout.activity_authentication);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
+        Button logInButton = (Button) findViewById(R.id.loginButton);
+        Button signUpButton = (Button) findViewById(R.id.signupButton);
+
+        logInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent loginActivity = new Intent(v.getContext(), LogInActivity.class);
+                startActivity(loginActivity);
+            }
+        });
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signupActivity = new Intent(v.getContext(), SignUpActivity.class);
+                startActivity(signupActivity);
+            }
+        });
 
     }
 
